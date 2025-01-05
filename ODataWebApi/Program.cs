5fa5f343e1c04cf9aa5005d337aaa5b82ca88225
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-05 12:44:32 +0300

    scaler den ayağa kaldırıldı.

--- ODataWebApi/Program.cs
+++ ODataWebApi/Program.cs
@@ -1,4 +1,6 @@
 
+using Scalar.AspNetCore;
+
 namespace ODataWebApi
 {
     public class Program
@@ -19,6 +21,7 @@ namespace ODataWebApi
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
